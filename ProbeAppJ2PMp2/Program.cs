d96e4e5b183512efaf473cefca0227d8b8ff5162
Corentin Z <corentinz@outlook.fr>
2024-01-09 15:06:25 +0100

    Upload J2 PM p2

---
+++ ProbeAppJ2PMp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using ProbeLibraryJ2PMp2;
+
+namespace ProbeAppJ2PMp2
+{
+    internal class Program
+    {
+        public static Random RAND = new();
+        static void Main(string[] args)
+        {
+            ClassUsingHandler handler = new();
+            handler.IntHandler += TestMethod;
+
+            Console.WriteLine(handler.callHandler());
+        }
+
+        static int TestMethod() => RAND.Next(10, 100);
+    }
+}
---
+++ ProbeLibrary/ClassUsingHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace ProbeLibraryJ2PMp2
+{
+    public delegate int ReturningIntHandler();
+
+    public class ClassUsingHandler
+    {
+        public event ReturningIntHandler IntHandler;
+
+        public int callHandler() { return IntHandler(); }
+    }
+}
